f66275f8a076aec3ea8ca1aacd23f66b0ca84da8
Ranish Maharjan <ranishmaharjan@gmail.com>
2022-07-12 17:27:59 +0545

    [add] header, cart, meal components and displaying each meal item

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import AvailableMeals from "./AvaialbleMeals";
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvailableMeals />
+    </>
+  );
+};
+
+export default Meals;
